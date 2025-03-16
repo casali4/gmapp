49edd1f49a526ccdf313771e38c176f0601b91d5
casali4 <office@felixkazda.com>
2025-03-16 23:03:24 +0100

    changes to landing

--- core/urls.py
+++ core/urls.py
@@ -10,5 +10,7 @@ from . import views
 urlpatterns = [
 
     path('', views.landing, name='landing'),
+    path('contact', views.contact, name='contact'),
+    path('sign-in', views.sign_in, name='sign_in'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def landing(request):
     return render(request, 'landing.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def sign_in(request):
+    return render(request, 'sign-in.html')
